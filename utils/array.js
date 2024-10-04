117b18e9a854c35e32a203fb3ce582b7bb490a88
Glenn de Haan <glenn@dehaan.cloud>
2024-10-04 13:14:22 +0200

    Updated deprecation token list

--- utils/array.js
+++ utils/array.js
@@ -5,6 +5,8 @@ module.exports = {
     deprecated: [
         'SECURITY_CODE',
         'DISABLE_AUTH',
-        'AUTH_OIDC_CLIENT_TYPE'
+        'AUTH_OIDC_CLIENT_TYPE',
+        'AUTH_PASSWORD',
+        'AUTH_TOKEN'
     ]
 };
