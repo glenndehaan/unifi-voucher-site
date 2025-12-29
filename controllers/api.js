a61be4487e16834fa4a4e8cad1ed50fc3ae21d02
nickelblack619 <79773045+nickelblack619@users.noreply.github.com>
2025-12-29 18:40:03 +0000

    Revert "Update api.js"
    This reverts commit 6cfae61e6db4745b09e920dd339b4a8f18dbf498.

--- controllers/api.js
+++ controllers/api.js
@@ -12,6 +12,7 @@ const mail = require('../modules/mail');
 const {updateCache} = require('../utils/cache');
 const types = require('../utils/types');
 const languages = require('../utils/languages');
+const notes = require('../utils/notes');
 
 module.exports = {
     api: {
@@ -107,6 +108,7 @@ module.exports = {
                 data: {
                     message: 'OK',
                     vouchers: cache.vouchers.map((voucher) => {
+                        const parsedNotes = notes(voucher.name);
                         return {
                             id: voucher.id,
                             code: `${voucher.code.slice(0, 5)}-${voucher.code.slice(5)}`,
