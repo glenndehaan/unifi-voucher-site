3a9c3607dbee49a6bc0bf45b695e1795e8fb595a
brandong-oly <brandong@olypensupport.com>
2026-07-15 09:19:45 -0700

    Update modules/variables.js
    Co-authored-by: Glenn de Haan <glenn@dehaan.cloud>

--- modules/variables.js
+++ modules/variables.js
@@ -17,7 +17,7 @@ module.exports = {
     unifiToken: config('unifi_token') || process.env.UNIFI_TOKEN || '',
     unifiSiteManagerConsoleId: config('unifi_site_manager_console_id') || process.env.UNIFI_SITE_MANAGER_CONSOLE_ID || '',
     unifiSiteId: config('unifi_site_id') || process.env.UNIFI_SITE_ID || 'default',
-    unifiSiteMgr: config('unifi_site_mgr') || process.env.UNIFI_SITE_MGR || 'false',
+    unifiSiteManager: config('unifi_site_manager') || process.env.UNIFI_SITE_MANAGER || 'false',
     unifiSsid: config('unifi_ssid') || process.env.UNIFI_SSID || '',
     unifiSsidPassword: config('unifi_ssid_password') || process.env.UNIFI_SSID_PASSWORD || '',
     voucherTypes: config('voucher_types') || process.env.VOUCHER_TYPES || '480,1,,,;',
